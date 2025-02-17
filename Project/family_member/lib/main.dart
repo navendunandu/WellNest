4cf3af9d14129df1dc511aacd3477bbea7f96b44
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-17 18:41:02 +0530

    Connections and navigation

--- Project/family_member/lib/main.dart
+++ Project/family_member/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:family_member/screens/landingpage.dart';
+import 'package:family_member/screens/residentregistration.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://xigidxtugemqjqxxupbq.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZ2lkeHR1Z2VtcWpxeHh1cGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxODc0NTksImV4cCI6MjA1NDc2MzQ1OX0._nRKz4mIaREHLmv5mihabn9kwI180MBZIxwHbqkklQ8',
+  );
+
+  runApp(MainApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -11,7 +20,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Landingpage()
+      home: Residentregistration()
     );
   }
 }
