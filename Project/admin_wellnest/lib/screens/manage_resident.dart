a0eebd3f069fca57b7d7193c1851176da83bbe50
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-19 11:15:35 +0530

    Refactor ManageResident widget to StatefulWidget and update display text

--- Project/admin_wellnest/lib/screens/manage_resident.dart
+++ Project/admin_wellnest/lib/screens/manage_resident.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
-class ManageResident extends StatelessWidget {
+class ManageResident extends StatefulWidget {
   const ManageResident({super.key});
+
+  @override
+  State<ManageResident> createState() => _ManageResidentState();
+}
+
+class _ManageResidentState extends State<ManageResident> {
+  // ignore: unused_field
+ 
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Resident"));
-  }
+    return Center(
+      child: Text("Manage Resident"),
+    );
+     }
 }
