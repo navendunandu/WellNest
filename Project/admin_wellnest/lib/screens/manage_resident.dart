ec8d4f2a1c198d20d94a051bd6b5bf2a8badf30a
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-20 12:06:58 +0530

    Add resident management functionality with status updates and caretaker assignment

--- Project/admin_wellnest/lib/screens/manage_resident.dart
+++ Project/admin_wellnest/lib/screens/manage_resident.dart
@@ -1,4 +1,7 @@
+import 'package:admin_wellnest/main.dart';
+import 'package:admin_wellnest/screens/assign_caretaker.dart';
 import 'package:flutter/material.dart';
+
 class ManageResident extends StatefulWidget {
   const ManageResident({super.key});
 
@@ -7,13 +10,97 @@ class ManageResident extends StatefulWidget {
 }
 
 class _ManageResidentState extends State<ManageResident> {
-  // ignore: unused_field
- 
+  List<Map<String, dynamic>> _filetypeList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFiletype();
+  }
+
+  Future<void> fetchFiletype() async {
+    try {
+      final response =
+          await supabase.from('tbl_resident').select().gt('resident_status', 0).neq('resident_status', 2);
+      setState(() {
+        _filetypeList = response;
+      });
+    } catch (e) {
+      print("ERROR FETCHING FILE TYPE DATA: $e");
+    }
+  }
+  Future<void> updateResidentStatus(String residentId, int status) async {
+    try {
+      await supabase.from('tbl_resident').update(
+          {'resident_status': status}).match({'resident_id': residentId});
+      fetchFiletype();
+    } catch (e) {
+      print("ERROR UPDATING RESIDENT STATUS: $e");
+    }
+  }
+void paymentVerify(String residentId) {
+    updateResidentStatus(residentId, 3);
+  }
+  void assign() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => (AssignCaretaker())));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Manage Resident"),
-    );
-     }
+    return DataTable(
+        columns: const [
+          DataColumn(label: Text("Sl.No")),
+          DataColumn(label: Text("Resident_Name")),
+          DataColumn(label: Text("Room_ID")),
+          DataColumn(label: Text("Relation_ID")),
+          DataColumn(label: Text("Resident_Contact")),
+          DataColumn(label: Text("Resident_Email")),
+          DataColumn(label: Text("Action")),
+        ],
+        rows: _filetypeList.asMap().entries.map((entry) {
+          print(entry.value);
+          return DataRow(cells: [
+            DataCell(Text((entry.key + 1).toString())),
+            DataCell(Text(entry.value['resident_name'].toString())),
+            DataCell(Text(entry.value['room_id'].toString())),
+            DataCell(Text(entry.value['relation_id'].toString())),
+            DataCell(Text(entry.value['resident_contact'].toString())),
+            DataCell(Text(entry.value['resident_email'].toString())),
+            DataCell(
+              entry.value['resident_status'] == 1
+                  ? Row(
+                      children: [
+                        Text("Payment Pending.."),
+                        IconButton(
+                          icon: Icon(Icons.verified),
+                          onPressed: () {
+                            paymentVerify(entry.value['resident_id'].toString());
+                          },
+                        ),
+                      ],
+                    )
+                  : ElevatedButton(
+                      onPressed: () {
+                        assign();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 24, 56, 111),
+                        padding: EdgeInsets.symmetric(horizontal: 17, vertical: 8),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        "Assign",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+            ),
+          ]);
+        }).toList());
+  }
 }
