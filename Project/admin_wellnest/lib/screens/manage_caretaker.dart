632690f98821dab083c19e11e93cf88a7605ea0b
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-20 12:06:28 +0530

    `Updated multiple files with various changes, including new features, bug fixes, and UI improvements.`

--- Project/admin_wellnest/lib/screens/manage_caretaker.dart
+++ Project/admin_wellnest/lib/screens/manage_caretaker.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:admin_wellnest/components/form_validation.dart';
 import 'package:admin_wellnest/main.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 class ManageCaretaker extends StatefulWidget {
   const ManageCaretaker({super.key});
@@ -18,11 +22,23 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
   final formKey = GlobalKey<FormState>();
   bool isLoading = true;
   List<Map<String, dynamic>> caretaker = [];
+  PlatformFile? pickedImage;
 
   @override
   void initState() {
     super.initState();
     fetchData();
+  }
+
+  Future<void> handleImagePick() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: false, // Only single file upload
+    );
+    if (result != null) {
+      setState(() {
+        pickedImage = result.files.first;
+      });
+    }
   }
 
   Future<void> fetchData() async {
@@ -44,19 +60,41 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
     }
   }
 
-  Future<void> submit() async {
+  Future<void> register() async {
+    try {
+      final auth = await supabase.auth.signUp(
+          password: passwordController.text, email: emailController.text);
+      String uid = auth.user!.id;
+      submit(uid);
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future<void> submit(String uid) async {
     if (formKey.currentState!.validate()) {
       setState(() {
         isLoading = true;
       });
       try {
-        await supabase.from('tbl_caretaker').insert({
-          'caretaker_name': nameController.text,
-          'caretaker_email': emailController.text,
-          'caretaker_password': passwordController.text,
-          'caretaker_contact': phoneController.text,
-        });
-
+        String? url = await photoUpload(uid);
+        if (url!.isNotEmpty) {
+          await supabase.from('tbl_caretaker').insert({
+            'caretaker_id': uid,
+            'caretaker_name': nameController.text,
+            'caretaker_email': emailController.text,
+            'caretaker_password': passwordController.text,
+            'caretaker_contact': phoneController.text,
+            'caretaker_photo': url,
+          });
+        }
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+            "Caretaker details updated successfully!",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Color.fromARGB(255, 0, 40, 1),
+        ));
         print("Insert Successful");
 
         // Clear fields after successful submission
@@ -66,7 +104,9 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
         confirmPasswordController.clear();
         phoneController.clear();
         formKey.currentState!.reset();
-
+        setState(() {
+          pickedImage = null;
+        });
         // Refresh data after insert
         await fetchData();
       } catch (e) {
@@ -78,7 +118,26 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
       }
     }
   }
- Future<void> deleteRoom(int id) async {
+
+  Future<String?> photoUpload(String uid) async {
+    try {
+      final bucketName = 'ct_files'; // Replace with your bucket name
+      final filePath = "$uid-${pickedImage!.name}";
+      await supabase.storage.from(bucketName).uploadBinary(
+            filePath,
+            pickedImage!.bytes!, // Use file.bytes for Flutter Web
+          );
+      final publicUrl =
+          supabase.storage.from(bucketName).getPublicUrl(filePath);
+      // await updateImage(uid, publicUrl);
+      return publicUrl;
+    } catch (e) {
+      print("Error photo upload: $e");
+      return null;
+    }
+  }
+
+  Future<void> deletecaretaker(int id) async {
     try {
       await supabase.from('tbl_caretaker').delete().eq('caretaker_id', id);
       print("Delete Successful");
@@ -100,19 +159,29 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
       );
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromARGB(255, 227, 242, 253), 
-      padding: EdgeInsets.all(30), 
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 24, 56, 111),
+            Color.fromARGB(255, 251, 247, 179),
+          ],
+        ),
+      ),
+      padding: EdgeInsets.all(30),
       child: Center(
         child: SingleChildScrollView(
           child: Card(
-            elevation: 5,
+            elevation: 10,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
+              borderRadius: BorderRadius.circular(20),
             ),
-            color: Colors.white, 
+            color: Colors.white,
             margin: EdgeInsets.symmetric(horizontal: 100, vertical: 30),
             child: Padding(
               padding: const EdgeInsets.all(30),
@@ -124,9 +193,16 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
                     Text(
                       "Add Caretaker",
                       style: TextStyle(
-                        fontSize: 24,
+                        fontSize: 28,
                         fontWeight: FontWeight.bold,
                         color: Color.fromARGB(255, 24, 56, 111),
+                        shadows: [
+                          Shadow(
+                            blurRadius: 2,
+                            color: Colors.black12,
+                            offset: Offset(1, 1),
+                          )
+                        ],
                       ),
                     ),
                     SizedBox(height: 20),
@@ -163,9 +239,40 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
                         "Enter Contact Information",
                         Icons.phone,
                         (value) => FormValidation.validateContact(value)),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      height: 120,
+                      width: 120,
+                      child: pickedImage == null
+                          ? GestureDetector(
+                              onTap: handleImagePick,
+                              child: Icon(
+                                Icons.add_a_photo,
+                                color: Color(0xFF0277BD),
+                                size: 50,
+                              ),
+                            )
+                          : GestureDetector(
+                              onTap: handleImagePick,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: pickedImage!.bytes != null
+                                    ? Image.memory(
+                                        Uint8List.fromList(
+                                            pickedImage!.bytes!), // For web
+                                        fit: BoxFit.cover,
+                                      )
+                                    : Image.file(
+                                        File(pickedImage!
+                                            .path!), // For mobile/desktop
+                                        fit: BoxFit.cover,
+                                      ),
+                              ),
+                            ),
+                    ),
                     SizedBox(height: 20),
                     ElevatedButton(
-                      onPressed: submit,
+                      onPressed: register,
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Color.fromARGB(255, 24, 56, 111),
                         padding:
@@ -173,6 +280,8 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
+                        elevation: 5,
+                        shadowColor: Colors.black26,
                       ),
                       child: Text(
                         "Add",
@@ -188,9 +297,16 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
                     Text(
                       "Caretaker List",
                       style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
                         color: Color.fromARGB(255, 24, 56, 111),
+                        shadows: [
+                          Shadow(
+                            blurRadius: 2,
+                            color: Colors.black12,
+                            offset: Offset(1, 1),
+                          )
+                        ],
                       ),
                     ),
                     SizedBox(height: 20),
@@ -206,22 +322,46 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
                                   final data = caretaker[index];
                                   return Card(
                                     margin: EdgeInsets.symmetric(vertical: 10),
+                                    elevation: 5,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
                                     child: ListTile(
-                                      title: Text(data['name']),
+                                      title: Text(
+                                        data['caretaker_name'],
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      ),
                                       subtitle: Column(
                                         crossAxisAlignment:
                                             CrossAxisAlignment.start,
                                         children: [
-                                          Text("Email: ${data['email']}"),
-                                          Text("Contact: ${data['contact']}"),
-                                          IconButton(
-                                        icon: Icon(Icons.delete,
-                                            color: const Color.fromARGB(
-                                                255, 67, 4, 0)),
-                                        onPressed: () {
-                                          deleteRoom(data['caretaker_id']);
-                                        },
-                                      ),
+                                          Text(
+                                              "Email: ${data['caretaker_email']}"),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                  "Contact: ${data['caretaker_contact']}"),
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                    left: 10),
+                                                child: IconButton(
+                                                  icon: Icon(Icons.delete,
+                                                      color:
+                                                          const Color.fromARGB(
+                                                              255, 67, 4, 0)),
+                                                  onPressed: () {
+                                                    deletecaretaker(
+                                                        data['caretaker_id']);
+                                                  },
+                                                ),
+                                              ),
+                                            ],
+                                          ),
                                         ],
                                       ),
                                     ),
@@ -257,6 +397,8 @@ class _ManageCaretakerState extends State<ManageCaretaker> {
             borderSide:
                 BorderSide(color: Color.fromARGB(255, 24, 56, 111), width: 2),
           ),
+          filled: true,
+          fillColor: Colors.white.withOpacity(0.9),
         ),
       ),
     );
