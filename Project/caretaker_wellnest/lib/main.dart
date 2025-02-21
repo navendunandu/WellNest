72e289ee3850747cffce9fa74afeb679dd9073e0
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-21 09:21:57 +0530

    Update main app entry point to display ResidentProfile screen

--- Project/caretaker_wellnest/lib/main.dart
+++ Project/caretaker_wellnest/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:caretaker_wellnest/screens/homepage.dart';
 import 'package:caretaker_wellnest/screens/login_page.dart';
+import 'package:caretaker_wellnest/screens/resident_profile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Homepage()
+      home: ResidentProfile()
     );
   }
 }
