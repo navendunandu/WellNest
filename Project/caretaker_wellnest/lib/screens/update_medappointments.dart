9347ac5e80600ceff78ff68e26e92bb6ca7c234b
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-26 23:07:48 +0530

    Add UpdateMedappointments screen with placeholder widget

---
+++ Project/caretaker_wellnest/lib/screens/update_medappointments.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateMedappointments extends StatefulWidget {
+  const UpdateMedappointments({super.key});
+
+  @override
+  State<UpdateMedappointments> createState() => _UpdateMedappointmentsState();
+}
+
+class _UpdateMedappointmentsState extends State<UpdateMedappointments> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
