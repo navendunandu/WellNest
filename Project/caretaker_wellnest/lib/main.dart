9467f5f2dbd75650f79c329d22f13bc4945770c3
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-26 23:40:34 +0530

    Change home page to ResidentProfile

--- Project/caretaker_wellnest/lib/main.dart
+++ Project/caretaker_wellnest/lib/main.dart
@@ -23,7 +23,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginPage()
+      home: ResidentProfile()
     );
   }
 }
