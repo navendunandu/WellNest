ca1a86d6b3d9ae72df9075649097e1b3c4adb421
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-28 17:14:30 +0530

    Change ViewMedappointments from stateless to stateful widget for enhanced functionality

--- Project/family_member/lib/screens/view_medappointments.dart
+++ Project/family_member/lib/screens/view_medappointments.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ViewMedappointments extends StatelessWidget {
+class ViewMedappointments extends StatefulWidget {
   const ViewMedappointments({super.key});
 
+  @override
+  State<ViewMedappointments> createState() => _ViewMedappointmentsState();
+}
+
+class _ViewMedappointmentsState extends State<ViewMedappointments> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
