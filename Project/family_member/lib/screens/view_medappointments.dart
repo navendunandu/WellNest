d77c2acc30a83b65e3340e57902a464f7a68c86d
Ajith Mani <ajithambattuparambil@gmail.com>
2025-02-28 17:26:30 +0530

    Add Supabase integration to ViewMedappointments screen for fetching and displaying appointments

--- Project/family_member/lib/screens/view_medappointments.dart
+++ Project/family_member/lib/screens/view_medappointments.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ViewMedappointments extends StatefulWidget {
   const ViewMedappointments({super.key});
@@ -8,8 +9,56 @@ class ViewMedappointments extends StatefulWidget {
 }
 
 class _ViewMedappointmentsState extends State<ViewMedappointments> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  List<Map<String, dynamic>> appointments = [];
+
+  Future<void> fetchAppointments() async {
+    final response = await supabase.from('tbl_appointment').select();
+    setState(() {
+      appointments = response;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Color.fromARGB(230, 255, 252, 197),
+        appBar: AppBar(
+          title: const Text(
+            'View Appointments',
+            style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontWeight: FontWeight.bold,
+                fontSize: 23),
+          ),
+          backgroundColor: Color.fromARGB(255, 0, 36, 94),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  appointments.isEmpty
+                      ? const Center(child: Text("No Appointments Available"))
+                      : Expanded(
+                          child: ListView.builder(
+                            itemCount: appointments.length,
+                            itemBuilder: (context, index) {
+                              final appointment = appointments[index];
+                              return Card(
+                                margin: const EdgeInsets.symmetric(vertical: 8),
+                                child: ListTile(
+                                  title: Text(
+                                      "Dr. ${appointment['appointment_name']}"),
+                                  subtitle: Text(
+                                    "Date: ${appointment['appointment_date']}\n"
+                                    "Time: ${appointment['appointment_time']}",
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                ])));
   }
 }
