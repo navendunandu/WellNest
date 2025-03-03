86abbf16f7178d856ad305f230e596f54a0203c2
Ajith Mani <ajithambattuparambil@gmail.com>
2025-03-03 17:29:35 +0530

    Add ViewProfile screen to display user profile information

---
+++ Project/family_member/lib/screens/view_profile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ViewProfile extends StatefulWidget {
+  String profile;
+  ViewProfile({super.key, required this.profile});
+
+  @override
+  State<ViewProfile> createState() => _ViewProfileState();
+}
+
+class _ViewProfileState extends State<ViewProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
