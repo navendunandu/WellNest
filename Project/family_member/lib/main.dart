b0149e203652eda31b09129e19fcb83ce5764f8d
Ajith Mani <ajithambattuparambil@gmail.com>
2025-03-12 17:23:36 +0530

    Clean up main.dart by removing unused imports and formatting MaterialApp return statement

--- Project/family_member/lib/main.dart
+++ Project/family_member/lib/main.dart
@@ -1,15 +1,12 @@
-import 'package:family_member/screens/homepage.dart';
 import 'package:family_member/screens/landingpage.dart';
-import 'package:family_member/screens/payment.dart';
-import 'package:family_member/screens/view_health.dart';
-import 'package:family_member/screens/view_medappointments.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
     url: 'https://xigidxtugemqjqxxupbq.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZ2lkeHR1Z2VtcWpxeHh1cGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxODc0NTksImV4cCI6MjA1NDc2MzQ1OX0._nRKz4mIaREHLmv5mihabn9kwI180MBZIxwHbqkklQ8',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZ2lkeHR1Z2VtcWpxeHh1cGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxODc0NTksImV4cCI6MjA1NDc2MzQ1OX0._nRKz4mIaREHLmv5mihabn9kwI180MBZIxwHbqkklQ8',
   );
 
   runApp(MainApp());
@@ -22,8 +19,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Landingpage()
-    );
+    return const MaterialApp(home: Landingpage());
   }
 }
