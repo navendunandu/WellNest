04b08a8e84e27336b2476aecf8b53f3ee6bd8930
Ajith Mani <ajithambattuparambil@gmail.com>
2025-03-16 14:16:26 +0530

    Add FamProfile screen with initial placeholder implementation

---
+++ Project/family_member/lib/screens/fam_profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FamProfile extends StatefulWidget {
+  const FamProfile({super.key});
+
+  @override
+  State<FamProfile> createState() => _FamProfileState();
+}
+
+class _FamProfileState extends State<FamProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
